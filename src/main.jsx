1b0b8be98328584715aa4c1f628bde62baac9ef4
robin kumawat <kumawatrobin7@gmail.com>
2024-03-12 16:52:33 +0530

    add youtube api

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Videos from './Component/Home/Videos/Video.jsx'
+import Video from './Component/Home/Videos/Video.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Videos/>
+    <Video/>
   </React.StrictMode>,
 )
